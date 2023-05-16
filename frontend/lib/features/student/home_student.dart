d7d8e92621f15bdaae955c047d7e4910263b5b1d
Wilhelm Steins <49406650+wilhelmSt@users.noreply.github.com>
2023-05-16 08:30:58 -0300

    Create home student

---
+++ frontend/lib/features/student/home_student.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenStudent extends StatefulWidget {
+  const HomeScreenStudent({super.key});
+
+  @override
+  State<HomeScreenStudent> createState() => _HomeScreenStudentState();
+}
+
+class _HomeScreenStudentState extends State<HomeScreenStudent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
